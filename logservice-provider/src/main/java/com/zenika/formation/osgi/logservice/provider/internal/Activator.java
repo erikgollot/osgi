ba1260ffd63cf63952e27024b81b93b2529a347b
erikgollot <erik_gollot@yahoo.fr>
2014-12-08 14:46:31 +0100

    i

--- logservice-provider/src/main/java/com/zenika/formation/osgi/logservice/provider/internal/Activator.java
+++ logservice-provider/src/main/java/com/zenika/formation/osgi/logservice/provider/internal/Activator.java
@@ -1,5 +1,31 @@
 package com.zenika.formation.osgi.logservice.provider.internal;
 
-public class Activator {
-    
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.log.LogService;
+
+public class Activator implements BundleActivator {
+	BasicLogService service;
+	ServiceRegistration<LogService> sr = null;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		service = new BasicLogService();
+		Dictionary<String, String> props = new Hashtable<String, String>();
+		props.put("nature", "basic");
+		sr = context.registerService(LogService.class, service, props);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		if (sr != null) {
+			sr.unregister();
+		}
+
+	}
+
 }
--- logservice-provider/src/main/java/com/zenika/formation/osgi/logservice/provider/internal/BasicLogService.java
+++ logservice-provider/src/main/java/com/zenika/formation/osgi/logservice/provider/internal/BasicLogService.java
@@ -1,5 +1,30 @@
 package com.zenika.formation.osgi.logservice.provider.internal;
 
-public class BasicLogService {
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogService;
+
+public class BasicLogService  implements LogService{
+
+	@Override
+	public void log(int level, String message) {
+		System.out.println("log "+level+" : "+message);
+	}
+
+	@Override
+	public void log(int level, String message, Throwable exception) {
+		System.out.println("log "+level+" : "+message);
+	}
+
+	@Override
+	public void log(ServiceReference sr, int level, String message) {
+		System.out.println("log "+level+" : "+message);
+		
+	}
+
+	@Override
+	public void log(ServiceReference sr, int level, String message,
+			Throwable exception) {
+		System.out.println("log "+level+" : "+message);
+	}
 
 }
